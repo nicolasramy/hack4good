f67d9b777404e92d4ee3b2e77b3d38eaac5b09d8
Raphael Cherrier <speedraf@yahoo.fr>
2013-10-05 21:39:39 +0200

    fix conflict

--- backoffice/api.py
+++ backoffice/api.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
-from flask.ext import restful
+from flask import Flask, request
+from flask.ext.restful import Resource, Apis
 
 app = Flask(__name__)
-api = restful.Api(app)
+api = Api(app)
 
 
-class HelloWorld(restful.Resource):
+class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
+
+
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
